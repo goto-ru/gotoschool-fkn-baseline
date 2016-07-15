2d4d06a9aac76781afd94c42b821ae056c15a552
Dmitriy Volkov <wldhx@wldhx.me>
2016-07-15 20:06:33 +0300

    Metrics: swap own `apk()` impl with external
    The external one is recommended by Kaggle as reference: https://www.kaggle.com/wiki/MeanAveragePrecision
    Link: https://github.com/kmbnw/Metrics/blob/67b0684f433b91530f7a71bbfd78a8295834862d/Python/ml_metrics/average_precision.py
    
    Fixes #7

--- metrics.py
+++ metrics.py
@@ -1,15 +1,38 @@
+# via https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
+# Copyright (c) 2012, Ben Hamner (ben@benhamner.com)
 def apk(actual, predicted, k=10):
-    """Get Average Precision at k."""
-    count_relevants = 0
-    sum_of_precisions = 0.0
-    
-    for i in range(min(k, len(predicted))):
-        currentk = i + 1.0
-        
-        if predicted[i] in actual:
-            count_relevants += 1
-        
-        precision_at_k = count_relevants / currentk 
-        sum_of_precisions += precision_at_k
-        
-    return sum_of_precisions / k
+    """
+    Computes the average precision at k.
+    This function computes the average prescision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+    """
+    if actual is None or len(actual) == 0:
+        return 0.0
+
+    if predicted is None or len(predicted) == 0:
+        return 0.0
+
+    if len(predicted)>k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    return score / min(len(actual), k)
